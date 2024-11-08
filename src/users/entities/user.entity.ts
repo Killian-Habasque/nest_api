cb99a8ac0dcc8db0dc0ff12e0859ca240f7a9c82
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-11-08 12:59:55 +0100

    [ADD]: website setup database, and relation with user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,10 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn, OneToMany } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
+import { Website } from 'src/websites/entities/website.entity';
 
 @Entity()
 @Unique(['username'])
 export class User {
+
+  @ApiProperty()
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @ApiProperty()
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
+
   @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
@@ -16,6 +26,9 @@ export class User {
   @Exclude()
   @Column({ length: 150, default: null })
   password: string;
+
+  @OneToMany(() => Website, website => website.user)
+  websites: Website[];
 
   constructor(partial: Partial<User>) {
     Object.assign(this, partial);
---
+++ src/websites/dto/create-website.dto.ts
@@ -0,0 +1,4 @@
+export class CreateWebsiteDto {
+    url: string;
+    title: string;
+}
